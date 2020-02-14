f0a8bba2b104bee0d84e47da478f5de8d0844bb2
Migue Eduardo Castillo Landeros <36210519+meclajer@users.noreply.github.com>
2020-02-14 17:47:46 -0600

    Programa 5
    No se entendio la solicitud.

---
+++ Programa_5/src/Principal.scala
@@ -0,0 +1,22 @@
+import scala.io.StdIn._
+
+/*
+  5.- Programa que muestre cuantos son los divisores enteros entre dos números dados.
+ */
+
+object Principal {
+  def main(args: Array[String]): Unit = {
+    print("\n\tDIVISORES ENTRE DOS NÚMEROS\nNumero 1: ")
+    var numeroUno = readInt()
+    print("Numero 2: ")
+    var numeroDos = readInt()
+    divisores(numeroUno,numeroDos)
+  }
+
+  def divisores(uno: Int, dos: Int): Unit = {
+    /*
+      No entendi lo que solicita el programa.
+     */
+  }
+
+}
